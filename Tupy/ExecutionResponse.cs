bec759c42a6d15028356212cd7459803eda36d97
geovanifg <geofergon@msn.com>
2018-10-16 20:40:52 -0300

    Inheritance change

--- Tupy/ExecutionResponse.cs
+++ Tupy/ExecutionResponse.cs
@@ -2,6 +2,20 @@
 {
     public class ExecutionResponse : ExecutionResponse<string>
     {
-        
+        public ExecutionResponse() : base(true)
+        {
+
+        }
+
+        public ExecutionResponse(bool success) : base(success, null)
+        {
+
+        }
+
+        public ExecutionResponse(bool success, string message)
+        {
+            IsSuccess = success;
+            Message = message;
+        }
     }
 }
